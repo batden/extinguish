04a06b65ec8da2e1e62032d1d882a1a1a0a6add7
Philippe Jean Guillaumie <batden@sfr.fr>
2022-08-12 13:37:35 +0200

    Add comments

--- extinguish.sh
+++ extinguish.sh
@@ -2,6 +2,9 @@
 
 # This Bash script helps you cleanly uninstall Enlightenment and related applications.
 
+# Always use the uninstall script matching the version of the script that was used
+# to do the installation!
+
 # EXTINGUISH is written and maintained by batden@sfr.fr, feel free to
 # use this script as you see fit.
 
