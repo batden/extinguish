b30466960ffbd6f13ba4354049252a4994f72208
Philippe Jean Guillaumie <batden@sfr.fr>
2023-12-15 11:52:17 +0100

    Some rewording

--- extinguish.sh
+++ extinguish.sh
@@ -65,7 +65,8 @@ uninstall_e26() {
 
   clear
   printf "\n\n$BDR%s %s\n\n" "* UNINSTALLING ENLIGHTENMENT DESKTOP ENVIRONMENT *"
-  printf "$BDR%s $OFF%s\n\n" "This may take a few minutes..."
+  printf "$BDR%s %s\n\n" "This may take a few minutes."
+  printf "$BLDR%s $OFF%s\n\n" "You will be prompted to answer some basic questions..."
   sleep 1
 
   cd $HOME
