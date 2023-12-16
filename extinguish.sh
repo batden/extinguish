a27b925c21b7e09291ac19b4f5d6b2b087f70266
Philippe Jean Guillaumie <batden@sfr.fr>
2023-12-16 08:07:45 +0100

    Fix typos

--- extinguish.sh
+++ extinguish.sh
@@ -94,7 +94,7 @@ uninstall_e26() {
   sudo rm -rf evas*
 
   cd /usr/local/bin
-  sudo rm -rf e_paledit*
+  sudo rm -rf enlightenment_paledit*
   sudo rm -rf eina*
   sudo rm -rf efl*
   sudo rm -rf elua*
@@ -284,7 +284,7 @@ uninstall_e26() {
   sudo sed -i '/entice/d' mimeinfo.cache &>/dev/null
   sudo sed -i '/ephoto/d' mimeinfo.cache &>/dev/null
   sudo sed -i '/rage/d' mimeinfo.cache &>/dev/null
-  sudo rm -rf e_paledit.desktop
+  sudo rm -rf enlightenment_paledit.desktop
   sudo rm -rf terminology.desktop
 
   cd /usr/local/share/doc
