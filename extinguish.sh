2a8787307f19c5b106f66077aa9d5a7894be4e3d
Philippe Jean Guillaumie <batden@sfr.fr>
2023-12-31 17:09:17 +0100

    Minor correction

--- extinguish.sh
+++ extinguish.sh
@@ -325,7 +325,7 @@ lo() {
   printf "\n\n$BDR%s $OFF%s\n\n" "All done."
   # Candidates for further deletion: Search for “extinguish”, “ebackups” and “pbackups” in your home folder.
   # Note that the binary dependencies (dev packages) are kept for system consistency. There is no safe way
-  # to remove them automatically, without close user supervision.
+  # to remove them automatically, without close supervision by the user.
 }
 
 lo
