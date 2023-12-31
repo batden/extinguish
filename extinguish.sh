d48c453ec75256fbef63242e665b44f99a1a2e9d
Philippe Jean Guillaumie <batden@sfr.fr>
2023-12-31 12:12:01 +0100

    Add targets to remov_preq()

--- extinguish.sh
+++ extinguish.sh
@@ -47,6 +47,7 @@ remov_preq() {
       cd $ESRC/rlottie
       sudo ninja -C build uninstall
       cd .. && rm -rf rlottie
+      sudo rm -rf /usr/local/lib/x86_64-linux-gnu/pkgconfig/rlottie.pc
       echo
       ;;
     n | N)
@@ -57,6 +58,7 @@ remov_preq() {
       echo
       sudo ninja -C build uninstall
       cd .. && rm -rf rlottie
+      sudo rm -rf /usr/local/lib/x86_64-linux-gnu/pkgconfig/rlottie.pc
       echo
       ;;
     esac
@@ -103,9 +105,6 @@ del_list() {
   sudo rm -rf -- *-1
   sudo rm -rf enlightenment
   sudo rm -rf express-0
-
-  cd /usr/local/lib/cmake
-  sudo rm -rf ddcutil
 
   cd /usr/local/lib/x86_64-linux-gnu
   sudo rm -rf ecore*
@@ -163,10 +162,8 @@ del_list() {
   cd /usr/local/lib/x86_64-linux-gnu/pkgconfig
   sudo rm -rf ecore*
   sudo rm -rf efl*
-  sudo rm -rf rlottie*
 
   cd /usr/local/share
-  sudo rm -rf ddcutil*
   sudo rm -rf ecore*
   sudo rm -rf ecrire*
   sudo rm -rf edi*
